2ee81c0eb819a565b3cf9e72517236e2f9794c3f
Didier Thomas <didierthomas.m@gmail.com>
2023-02-07 12:13:15 -0600

    Hasta la clase 19

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'AppCoder/templates/AppCoder'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
